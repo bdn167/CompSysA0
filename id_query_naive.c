64ea7d6955de656e6f38a2629295cbee753cec6a
bdn167 <bdn167@alumni.ku.dk>
2026-09-22 14:12:40 +0200

    maybe progress

--- id_query_naive.c
+++ id_query_naive.c
@@ -15,18 +15,20 @@ struct naive_data {
 };
 
 struct naive_data* mk_naive(struct record* rs, int n) {
-  // TODO
-  assert(0);
+  struct naive_data* data = malloc(sizeof(struct naive_data));
+  
+  data -> rs = rs + n;
+  data -> n = n;
+
+  return data;
 }
 
 void free_naive(struct naive_data* data) {
-  // TODO
-  assert(0);
+  free(data);
 }
 
 const struct record* lookup_naive(struct naive_data *data, int64_t needle) {
-  // TODO
-  assert(0);
+  
 }
 
 int main(int argc, char** argv) {
